1b2440994ab36b5896f7b57049343691b69b5381
Miguel Yarza <miguel.yarza@gmail.com>
2014-10-02 05:11:53 -0400

    Creados scripts

---
+++ scripts/aliases.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+alias la='ls -a'
+alias ll='ls -al'
+alias l='less'
+
+alias gs='git status'
+
+
---
+++ scripts/git.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:MiguelYarza/curso.git
+git push -u origin master
+
